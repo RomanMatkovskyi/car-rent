af5f0b4c19dbc48dc85998865f30a72919e71303
RomanMatkovskyi <121770356+RomanMatkovskyi@users.noreply.github.com>
2024-03-06 23:14:35 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
-        <BrowserRouter basename="/react_vite">
+        <BrowserRouter basename="/car_rent">
           <App />
         </BrowserRouter>
       </PersistGate>
